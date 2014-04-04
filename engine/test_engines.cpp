4e0f2cf6a30e91a2858123825f6ae060bf022385
def.gsus- <stefan.berke@modular-audio-graphics.com>
2014-04-04 16:57:39 +0200

    some statistics made - strange: not all wins-per-start-square are symmetric !! (must be a bug)

--- engine/test_engines.cpp
+++ engine/test_engines.cpp
@@ -18,6 +18,8 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
 
 ****************************************************************************/
 
+#include <iomanip>
+
 #include "board.h"
 #include "boardhelper.h"
 #include "search.h"
@@ -39,7 +41,9 @@ public:
             num_moves,
             num_invalid_moves,
             num_max_moves_reached,
-            num_moves_histogram[MAX_MOVES+1];
+            num_moves_histogram[MAX_MOVES+1],
+
+            num_wins_per_square[maxBoardSize*maxBoardSize*2];
     };
     Stats stat;
 
@@ -48,10 +52,10 @@ public:
         :   b(5,4),
             bh(b)
     {
-        maxDepth[0] = maxDepth[1] = 4;
+        maxDepth[0] = maxDepth[1] = 2;
 
         do_50_start = false;
-        do_random_first_move = true;
+        do_fixed_first_move = true;
     }
 
 
@@ -66,15 +70,19 @@ public:
                 b.flipStm();
         }
 
-        // start with random move
-        if (do_random_first_move)
+        int start_square = -1;
+
+        // start with fixed move
+        if (do_fixed_first_move)
         {
-            b.makeMove(rand()%(b.size()*b.size()));
-            b.flipStm();
+            b.makeMove(start_square = stat.num_games % (b.size()*b.size()));
             stat.num_moves++;
+            b.flipStm();
         }
 
-#define TTT_STOP { playing = false; continue; }
+        // ---- game loop ----
+
+        #define TTT_STOP { playing = false; continue; }
 
         bool playing = true;
         while (playing)
@@ -90,6 +98,10 @@ public:
                 stat.num_invalid_moves++;
                 TTT_STOP
             }
+
+            // store first move
+            if (start_square<0)
+                start_square = m;
 
             // -- make move --
 
@@ -113,44 +125,93 @@ public:
             if (bh.isWin(b, b.stm()))
             {
                 stat.num_wins[stm]++;
+                if (start_square>=0)
+                    stat.num_wins_per_square[start_square*2+stm]++;
                 TTT_STOP;
             }
 
             b.flipStm();
         }
-#undef TTT_STOP
+        #undef TTT_STOP
 
         stat.num_games++;
         stat.num_moves_histogram[b.numPly()]++;
     }
 
+
+
     void printStats(std::ostream& out = std::cout)
     {
+        float winx = 0, avmove = 0;
+        if (stat.num_wins[0] + stat.num_wins[1] > 0)
+            winx = (float)stat.num_wins[0] / (stat.num_wins[0] + stat.num_wins[1]) * 100;
+        if (stat.num_games>0)
+            avmove = (float)stat.num_moves / stat.num_games;
+
+        // settings
+        out << "board " << b.size() << " " << b.consecutives()
+            << "  depth " << maxDepth[0] << ":" << maxDepth[1] << "\n"
+            << "start-side " << (do_50_start? "X/O" : "X")
+            << "  fixed-first-move " << (do_fixed_first_move? "yes" : "no")
+            << "\n";
+        // stats
         out << "games " << stat.num_games
-            << " X=" << stat.num_wins[0] << " O=" << stat.num_wins[1]
-            << " (" << stat.num_draws << " draws)\n"
+            << "  wins " << stat.num_wins[0] << ":" << stat.num_wins[1]
+            << " (" << winx << "%)"
+            << "  draws " << stat.num_draws
+            << "\n"
             << "moves " << stat.num_moves
-            << " average " << ((float)stat.num_moves / stat.num_games)
-            << " invalid " << (stat.num_invalid_moves) << "\n"
-            << std::endl;
+            << "  average " << avmove
+            << "  invalid " << (stat.num_invalid_moves) << "\n"
+            << "wins per start square:\n";
+        for (size_t j=0; j<b.size(); ++j)
+        {
+            for (size_t i=0; i<b.size(); ++i)
+            {
+                const int winx = stat.num_wins_per_square[(j*b.size()+i)*2],
+                          wino = stat.num_wins_per_square[(j*b.size()+i)*2+1];
+                if (winx)
+                    out << std::right << std::setw(5) << winx << ":";
+                else
+                    out << std::right << std::setw(5) << " " << ":";
+                if (wino)
+                    out << std::left << std::setw(4) << wino << std::right;
+                else
+                    out << std::left << std::setw(4) << " " << std::right;
+            }
+            out << "\n";
+        }
+
+        out << std::endl;
     }
 
-
-    void run()
+    void run_test()
     {
         bh.setSize(b);
         memset(&stat, 0, sizeof(stat));
 
-        for (int i=0; i<500; ++i)
+        //const int num = 3000;
+        const int num = b.size() * b.size();
+        for (int i=0; i<num; ++i)
         {
-            if (i!=0 && i%10==0)
-                printStats();
+            //if (i!=0 && i%10==0)
+            //    printStats();
 
             play_game();
 
         }
 
         printStats();
+    }
+
+    void run()
+    {
+        for (int i=2; i<=10; ++i)
+        {
+            maxDepth[0] = i;
+            maxDepth[1] = 4;
+            run_test();
+        }
     }
 
     // ----------- public member ------------
@@ -164,7 +225,7 @@ public:
     int maxDepth[2];
     bool
         do_50_start,
-        do_random_first_move
+        do_fixed_first_move;
     ;
     // --- stats ----
 
@@ -182,3 +243,144 @@ void test_engines()
     test.run();
 
 }
+
+
+
+/*
+
+depth 4:4  start side X  random first move yes
+games 3000 wins 1075:1925 (35.8333%) draws 0
+moves 95601 average 31.867 invalid 0
+wins per start square:
+  110:0       0:123   130:0       0:132   132:0
+    0:120     0:110     0:122     0:117     0:118
+  114:0       0:111   118:0       0:115   117:0
+    0:125     0:129     0:124     0:124     0:113
+  109:0       0:141   126:0       0:101   119:0
+
+depth 5:5  start side X  random first move yes
+games 2380 wins 1725:655 (72.479%) draws 0
+moves 69137 average 29.0492 invalid 0
+wins per start square:
+   96:0      96:0      96:0     107:0     106:0
+   96:0       0:83      0:100   103:0      85:0
+   83:0       0:86     95:0       0:89      0:98
+   99:0       0:106     0:93     91:0      96:0
+   87:0     116:0     104:0      75:0      94:0
+
+
+
+board 5 4  depth 2:2
+start-side X  fixed-first-move yes
+games 25  wins 25:0 (100%)  draws 0
+moves 507  average 20.28  invalid 0
+wins per start square:
+    1:0       1:0       1:0       1:0       1:0
+    1:0       1:0       1:0       1:0       1:0
+    1:0       1:0       1:0       1:0       1:0
+    1:0       1:0       1:0       1:0       1:0
+    1:0       1:0       1:0       1:0       1:0
+
+board 5 4  depth 3:3
+start-side X  fixed-first-move yes
+games 25  wins 7:18 (28%)  draws 0
+moves 519  average 20.76  invalid 0
+wins per start square:
+    0:1       0:1       1:0       0:1       1:0
+    0:1       0:1       0:1       0:1       0:1
+    1:0       0:1       1:0       0:1       1:0
+    0:1       0:1       0:1       0:1       0:1
+    0:1       0:1       1:0       0:1       1:0
+
+board 5 4  depth 4:4
+start-side X  fixed-first-move yes
+games 25  wins 9:16 (36%)  draws 0
+moves 795  average 31.8  invalid 0
+wins per start square:
+    1:0       0:1       1:0       0:1       1:0
+    0:1       0:1       0:1       0:1       0:1
+    1:0       0:1       1:0       0:1       1:0
+    0:1       0:1       0:1       0:1       0:1
+    1:0       0:1       1:0       0:1       1:0
+
+board 5 4  depth 5:5
+start-side X  fixed-first-move yes
+games 25  wins 18:7 (72%)  draws 0
+moves 728  average 29.12  invalid 0
+wins per start square:
+    1:0       1:0       1:0       1:0       1:0
+    1:0       0:1       0:1       1:0       1:0
+    1:0       0:1       1:0       0:1       0:1
+    1:0       0:1       0:1       1:0       1:0
+    1:0       1:0       1:0       1:0       1:0
+
+board 5 4  depth 6:6
+start-side X  fixed-first-move yes
+games 25  wins 3:16 (15.7895%)  draws 0
+moves 3719  average 148.76  invalid 0
+wins per start square:
+    1:0       0:1       0:1       0:1       0:1
+    0:1       0:1       0:0       0:1       0:0
+    0:1       0:0       1:0       0:0       0:1
+    0:1       0:1       0:0       0:1       0:0
+    1:0       0:1       0:1       0:1       0:1
+
+
+
+
+board 5 4  depth 2:4
+start-side X  fixed-first-move yes
+games 25  wins 6:16 (27.2727%)  draws 3
+moves 732  average 29.28  invalid 0
+wins per start square:
+     :         :1       1:         :1        :1
+     :1        :1        :1        :1        :1
+    1:         :1       1:         :1       1:
+     :1        :1        :1        :1       1:
+     :         :1       1:         :1        :
+
+board 5 4  depth 3:4
+start-side X  fixed-first-move yes
+games 25  wins 3:22 (12%)  draws 0
+moves 617  average 24.68  invalid 0
+wins per start square:
+     :1        :1       1:         :1        :1
+     :1        :1        :1        :1        :1
+    1:         :1       1:         :1        :1
+     :1        :1        :1        :1        :1
+     :1        :1        :1        :1        :1
+
+board 5 4  depth 4:4
+start-side X  fixed-first-move yes
+games 25  wins 9:16 (36%)  draws 0
+moves 795  average 31.8  invalid 0
+wins per start square:
+    1:         :1       1:         :1       1:
+     :1        :1        :1        :1        :1
+    1:         :1       1:         :1       1:
+     :1        :1        :1        :1        :1
+    1:         :1       1:         :1       1:
+
+board 5 4  depth 5:4
+start-side X  fixed-first-move yes
+games 25  wins 21:4 (84%)  draws 0
+moves 645  average 25.8  invalid 0
+wins per start square:
+    1:        1:        1:        1:        1:
+    1:        1:         :1       1:        1:
+    1:         :1       1:         :1       1:
+    1:        1:         :1       1:        1:
+    1:        1:        1:        1:        1:
+
+board 5 4  depth 6:4
+start-side X  fixed-first-move yes
+games 25  wins 17:8 (68%)  draws 0
+moves 751  average 30.04  invalid 0
+wins per start square:
+    1:         :1       1:         :1       1:
+     :1       1:        1:        1:         :1
+    1:        1:        1:        1:        1:
+     :1       1:        1:        1:         :1
+    1:         :1       1:         :1       1:
+
+**/
